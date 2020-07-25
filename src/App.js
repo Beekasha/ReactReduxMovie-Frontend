c4c1f8791d9e32c7000e72789adf2fca536f8a5c
Beekasha <brian.j.kleiner@gmail.com>
2020-07-25 17:22:48 -0400

    fleshing out footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/Navbar'
+import Footer from './components/Footer';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Navbar /> 
+        <Navbar />
+        <Footer /> 
       </div>
       
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+            Thanks to: 
+            <a href="https://www.themoviedb.org/"> 
+                <div>TMDB</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
