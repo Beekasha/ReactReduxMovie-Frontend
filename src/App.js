c6c177ea8bf418c5faca1bc2ba97fb69324b1c62
Beekasha <brian.j.kleiner@gmail.com>
2020-07-25 16:49:28 -0400

    added fontawesome icon for template

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        App
-      </div>
+      // <div className="App">
+      //   App
+      // </div>
+      <span className="fas fa-ticket-alt fa-5x"></span>
     );
   }
 
