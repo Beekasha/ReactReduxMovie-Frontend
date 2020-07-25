96c11307384085787edefbfc2070338f2024772d
Beekasha <brian.j.kleiner@gmail.com>
2020-07-25 17:11:59 -0400

    fleshing out navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import Navbar from './components/Navbar'
+
 class App extends React.Component {
   render() {
     return (
-      // <div className="App">
-      //   App
-      // </div>
-      <span className="fas fa-ticket-alt fa-5x"></span>
+      <div className="App">
+        <Navbar /> 
+      </div>
+      
     );
   }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+
+function Navbar() {
+
+    return (
+        <div>
+          <nav className="navbar navbar-light bg-dark mb-5">
+            <div className="container">
+              <div className="navbar-header">
+                <a className="navbar-brand text-white text-lg brand-text" href="#">
+                  BK Movie App
+                </a>
+              </div>
+              <ul className="navbar-nav ml-auto text-light d-inline-block">
+                {/* header logos */}
+                <li className="nav-item d-inline-block mr-4">
+                  <i className="fas fa-film fa-5x" id="cinema-logo" />
+                </li>
+                <li className="nav-item d-inline-block mr-4"> 
+                  <i className="fab fa-react fa-5x" id="react-logo" />
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+    );
+}
+
+export default Navbar;
