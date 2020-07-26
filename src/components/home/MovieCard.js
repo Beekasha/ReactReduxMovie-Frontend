d2bea745e4c822b1bd006d193bfcf0569cdd8851
Beekasha <brian.j.kleiner@gmail.com>
2020-07-26 16:25:59 -0400

    rendering posters + movie data to screen. Implementing no-pic-available function

--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -2,35 +2,32 @@ import React, { Component } from 'react';
 
 //recieving key and movie
 class MovieCard extends Component {
+    // findImg = (imgUrl) => {
+    //     if (imgUrl === null) {
+    //         return {src='http://jbdev.nextmp.net/assets/images/posters/movie-poster-missing-2.jpg?v=1'}
+    //     }
+    // }
+
+
     render() {
         const movie = this.props.movie;
         console.log(this.props.movie)
         return (
-            // <div className="col-md-3 mb-5">
-            //     <div className="card card-body bg-dark text-center h-100">
-            //         <img className="w-100 mb-2" src={'image.tmdb.org/t/p/w500' + movie.results.poster_path} alt="Movie Cover" />
-            //         <img className="w-100 mb-2" src={'image.tmdb.org/t/p/w500' + movie.results.poster_path} alt="Movie Cover" />
-
-            //         <h5 className="text-light card-title">
-            //             {movie.results.title} - {movie.results.release_date}
-            //         </h5>
-            //         <a className="btn btn-primary" to={'/movie/' + movie.imdbID} />
-            //             Movie Details
-            //             <i className="fas fa-chevron-right" />
-                    
-            //     </div>
-            // </div>
             <div className="col-md-3 mb-5">
             <div className="card card-body bg-dark text-center h-100">
-                <img className="w-100 mb-2" src='Posterpath' alt="Movie Cover" />
+                <img 
+                    className="w-200 mb-2" 
+                    src={`http://image.tmdb.org/t/p/w200${movie.poster_path}`} 
+                
+                    alt="Movie Cover" />
 
                 <h5 className="text-light card-title">
-                    {movie.title} - Movie Date
+                    {/* release_date is slicing to pull out ONLY the year */}
+                    {movie.title} ({movie.release_date.slice(0, 4)})
                 </h5>
                 <a className="btn btn-primary" to={'/movie/' + "IMDB ID"} />
                     Movie Details
                     <i className="fas fa-chevron-right" />
-                
             </div>
         </div>
         );
@@ -38,3 +35,4 @@ class MovieCard extends Component {
 }
 
 export default MovieCard;
+
