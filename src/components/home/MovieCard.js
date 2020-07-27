6c8f16f40aca834f2f249a65e166c9464183f877
Beekasha <brian.j.kleiner@gmail.com>
2020-07-27 01:12:10 -0400

    new api rendering to home page

--- src/components/home/MovieCard.js
+++ src/components/home/MovieCard.js
@@ -6,28 +6,22 @@ class MovieCard extends Component {
 
 
     render() {
-        const movie = this.props.movie;
-        console.log(this.props.movie)
+        const { movie } = this.props;
         return (
-            <div className="col-md-3 mb-5">
+          <div className="col-md-3 mb-5">
             <div className="card card-body bg-dark text-center h-100">
-                <img 
-                    className="w-200 mb-2" 
-                    src="postersrc"               
-                    alt="Movie Cover" />
-
-                <h5 className="text-light card-title">
-                    {/* release_date is slicing to pull out ONLY the year */}
-                    {movie.title} - movie year
-                </h5>
-                <Link className="btn btn-primary" to={'/movie/' + movie.id}>
-                    Movie Details
-                    <i className="fas fa-chevron-right" />
-                </Link>
+              <img className="w-100 mb-2" src={movie.Poster} alt="Movie Cover" />
+              <h5 className="text-light card-title">
+                {movie.Title} ({movie.Year})
+              </h5>
+              <Link className="btn btn-primary" to={'/movie/' + movie.imdbID}>
+                Movie Details
+                <i className="fas fa-chevron-right" />
+              </Link>
             </div>
-        </div>
+          </div>
         );
-    }
+      }
 }
 
 export default MovieCard;
